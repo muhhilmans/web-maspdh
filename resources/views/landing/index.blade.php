74f444540149134b09e30867507a5ab7b2e206af
Muh Hilman Sholehudin <muhhilmansholehudin@gmail.com>
2024-11-21 07:15:33 +0700

    <update> [Landing]: Fix error

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1396,17 +1396,17 @@
   <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
-  <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
-  <script src="{{ asset('assets/vendor/glightbox//img/hero-img.pngjs/glightbox.min.js') }}"></script>
-  <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-  <script src="{{ asset('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
-  <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-  <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/landing/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/landing/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ asset('assets/landing/vendor/aos/aos.js') }}"></script>
+  <script src="{{ asset('assets/landing/vendor/glightbox//img/hero-img.pngjs/glightbox.min.js') }}"></script>
+  <script src="{{ asset('assets/landing/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+  <script src="{{ asset('assets/landing/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+  <script src="{{ asset('assets/landing/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ asset('assets/landing/vendor/swiper/swiper-bundle.min.js') }}"></script>
 
   <!-- Main JS File -->
-  <script src="{{ asset('assets/js/main.js') }}"></script>
+  <script src="{{ asset('assets/landing/js/main.js') }}"></script>
 
 </body>
 
