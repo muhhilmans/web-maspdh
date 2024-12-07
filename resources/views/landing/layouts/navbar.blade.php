0e5219f614c2437a80bbb1ac21c88be582db97f3
Muh Hilman Sholehudin <muhhilmansholehudin@gmail.com>
2024-12-07 08:27:15 +0700

    <update> [Landing]: Navbar

--- resources/views/landing/layouts/navbar.blade.php
+++ resources/views/landing/layouts/navbar.blade.php
@@ -2,24 +2,18 @@
     <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
         <a href="index.html" class="logo d-flex align-items-center me-auto">
-            <!-- Uncomment the line below if you also wish to use an image logo -->
-            <img src="assets/img/logo.png" alt="">
-            <h1 class="sitename">MASPDH</h1>
+            <img src="{{ asset('assets/img/logo-maspdh.png')}}" alt="">
+            {{-- <h1 class="sitename">MASPDH</h1> --}}
         </a>
 
         <nav id="navmenu" class="navmenu">
             <ul>
-                <li><a href="#hero" class="active">Home<br></a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#team">Team</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li class="dropdown"><a href="#"><span>Dropdown</span> <i
+                <li><a href="#hero" class="active">Beranda<br></a></li>
+                <li class="dropdown"><a href="#"><span>Tentang</span> <i
                             class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>
-                        <li><a href="#">Dropdown 1</a></li>
-                        <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i
+                        <li><a href="#">Sejarah Yayasan</a></li>
+                        {{-- <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i
                                     class="bi bi-chevron-down toggle-dropdown"></i></a>
                             <ul>
                                 <li><a href="#">Deep Dropdown 1</a></li>
@@ -28,13 +22,21 @@
                                 <li><a href="#">Deep Dropdown 4</a></li>
                                 <li><a href="#">Deep Dropdown 5</a></li>
                             </ul>
-                        </li>
-                        <li><a href="#">Dropdown 2</a></li>
-                        <li><a href="#">Dropdown 3</a></li>
-                        <li><a href="#">Dropdown 4</a></li>
+                        </li> --}}
+                        <li><a href="#">Sejarah Madrasah Aliyah</a></li>
+                        <li><a href="#">Pesan Pembina Yayasan</a></li>
+                        <li><a href="#">Pesan Kepala Madrasah</a></li>
                     </ul>
                 </li>
-                <li class="listing-dropdown"><a href="#"><span>Listing Dropdown</span> <i
+                <li class="dropdown"><a href="#"><span>Penerimaan</span> <i
+                            class="bi bi-chevron-down toggle-dropdown"></i></a>
+                    <ul>
+                        <li><a href="#">Cara Mendaftar</a></li>
+                        <li><a href="#">Biaya Pendaftaran</a></li>
+                    </ul>
+                </li>
+                <li><a href="blog.html">Berita</a></li>
+                {{-- <li class="listing-dropdown"><a href="#"><span>Listing Dropdown</span> <i
                             class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>
                         <li>
@@ -63,13 +65,13 @@
                             <a href="#">Column 5 link 3</a>
                         </li>
                     </ul>
-                </li>
-                <li><a href="#contact">Contact</a></li>
+                </li> --}}
+                <li><a href="#contact">Kontak</a></li>
             </ul>
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
 
-        <a class="btn-getstarted flex-md-shrink-0" href="index.html#about">Get Started</a>
+        <a class="btn-getstarted flex-md-shrink-0" href="index.html#about">Daftar Sekarang</a>
 
     </div>
 </header>
